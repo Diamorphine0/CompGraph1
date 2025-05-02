cbf248d300f3391bc8efd35ce0a4d49c957ca82b
Luka Barovic <barovic17@gmail.com>
2025-05-02 23:42:43 +0200

    Adding the triangle mesh

--- Simple obj file reader.cpp
+++ Simple obj file reader.cpp
@@ -4,30 +4,6 @@
 #include <algorithm>
 #include <vector>
 
-class Vector {
-public:
-	explicit Vector(double x = 0, double y = 0, double z = 0) {
-		data[0] = x;
-		data[1] = y;
-		data[2] = z;
-	}
-	double norm2() const {
-		return data[0] * data[0] + data[1] * data[1] + data[2] * data[2];
-	}
-	double norm() const {
-		return sqrt(norm2());
-	}
-	void normalize() {
-		double n = norm();
-		data[0] /= n;
-		data[1] /= n;
-		data[2] /= n;
-	}
-	double operator[](int i) const { return data[i]; };
-	double& operator[](int i) { return data[i]; };
-	double data[3];
-};
-
 Vector operator+(const Vector& a, const Vector& b) {
 	return Vector(a[0] + b[0], a[1] + b[1], a[2] + b[2]);
 }
